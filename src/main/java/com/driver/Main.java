3764c001a6591bcfbc107a4875aea282095ce846
vaishali-sharma <iitmgwl1975@gmail.com>
2023-03-26 15:46:07 +0530

    [task completed]

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+      RWOnly obj=new RWOnly();
+      //obj.name="object";
+      //System.out.println(obj.name);
+      obj.setName("RWOnly object");
+      System.out.print(obj.getName());
+  }
 }
